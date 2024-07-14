2b81ea30645f85893744a4a940ff8823ec099825
Diego Malta <diego_moreira.0791@outlook.com>
2024-07-13 23:19:25 -0300

    add file to .gitignore and git push to todolist-react repository

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [inputText, setInputText] = useState("")
+  const [list, setList] = useState([])
+
+  function addItem() {
+    inputText !== "" && setList(
+      [...list, inputText]
+    )
+    setInputText("")
+  }
+
+  function handleChange(event) {
+    setInputText(event.target.value)
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" name="listItem" value={inputText} onChange={handleChange} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {list.map(listValue => (
+            <li>{listValue}</li>
+          ))}
         </ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
